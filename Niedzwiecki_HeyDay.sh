b30ea393bb956d4e73e8f4d86e98ebf29062cc61
Isaac Niedzwiecki <71156945+isaacnied@users.noreply.github.com>
2020-10-10 10:46:25 -0500

    Added in script

---
+++ Niedzwiecki_HeyDay.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script will say "Hey!" to students in a personalized way for Hey Day
+
+# welcome the student and request name
+
+echo "Welcome! What is your name?"
+
+read name
+
+echo "Hey $name!"
+
+exit
